0a15d161c13b03373a48336d5ca95bcdf2489a51
Nguyen Thanh Long <longnt,22bi13264@usth.edu.vn>
2024-08-30 13:46:55 +0700

    lab3

---
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.edu.usth.weather.ui.theme.ForecastFragment;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+// Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.main, firstFragment).commit();
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i("Weather", "Start");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("Weather", "Pause");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("Weather", "Resume");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("Weather", "Stop");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather", "Destroy");
+    }
+}
